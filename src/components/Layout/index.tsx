8d7268b8926d4563c015d1bda4930039b8a43208
MarcelFeo <marcelfeo29@gmail.com>
2022-04-06 13:06:08 -0300

    adicionando o layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// styles
+import { Container, Wrapper } from './styles';
+// components
+import { Main } from '../Main';
+
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        {/* <MenuBar /> */}
+        <Main />
+        {/* <SideBar /> */}
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Layout;
