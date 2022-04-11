9e08bcb940aaf46391e3e3f2845880ccca09a05b
MarcelFeo <marcelfeo29@gmail.com>
2022-04-11 11:20:52 -0300

    finalizando a sidebar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,6 +4,7 @@ import { Container, Wrapper } from './styles';
 // components
 import Main from '../Main';
 import MenuBar from '../MenuBar';
+import SideBar from '../Sidebar';
 
 const Layout: React.FC = () => {
   return (
@@ -11,7 +12,7 @@ const Layout: React.FC = () => {
       <Wrapper>
         <MenuBar />
         <Main />
-        {/* <SideBar /> */}
+        <SideBar />
       </Wrapper>
     </Container>
   )
